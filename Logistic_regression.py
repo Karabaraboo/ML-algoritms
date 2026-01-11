efe0ca207fd8a22e689ec36f84a874b1d1bd07a4
Karabaraboo <toto-93@yandex.ru>
2026-01-11 15:26:28 +0300

    Создан метод fit и get_coef. Расчёт Loss, gradient и обновление весов.

--- Logistic_regression.py
+++ Logistic_regression.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 class MyLogReg():
     # Логистическая регрессия
     def __init__(self,
@@ -9,3 +12,33 @@ class MyLogReg():
     
     def __str__(self):
         return f"MyLogReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
+
+    def fit(self, X_df: pd.DataFrame, y_ser: pd.Series, verbose: bool=False):
+        X = np.column_stack((np.ones(X_df.shape[0]), X_df.to_numpy()))  # (N, N_feat+1)
+        y = y_ser.to_numpy()[:, np.newaxis]                             # (N, 1)
+        self.weights = np.ones((X.shape[1], 1))                         # (N_feat+1, 1)
+
+        n = X.shape[0]
+        for i in range(1, self.n_iter + 1):
+            # Предсказание модели
+            y_pred = 1 / (1 + np.exp(-np.matmul(X, self.weights)))      # (N, 1)
+
+            # Loss-функция
+            eps = 1e-15   # Чтобы избежать +-inf в логарифме
+            logLoss = -1 / n * np.sum(y * np.log(y_pred + eps) + (1 - y) * np.log(y_pred + eps))
+
+            # Градиент loss-функции
+            grad = 1 / n * np.matmul((y_pred - y).T, X)                 # (1, N_feat+1)
+
+            # Обновление весов
+            self.weights -= self.learning_rate * grad.T
+
+            # Вывод логов
+            if verbose:
+                if i == 1:
+                    print(f"start | loss: {logLoss}")
+                elif i % verbose == 0:
+                    print(f"{i} | loss: {logLoss}")
+
+    def get_coef(self):
+        return self.weights[1:]

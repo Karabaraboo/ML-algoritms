57f02d2c840eea50d79dc266ed2ae4e422e66473
Karabaraboo <toto-93@yandex.ru>
2026-01-11 13:49:55 +0300

    Создание класса MyLogReg

---
+++ Logistic_regression.py
@@ -0,0 +1,11 @@
+class MyLogReg():
+    # Логистическая регрессия
+    def __init__(self,
+                 n_iter=10,
+                 learning_rate=0.1):
+        self.n_iter = n_iter
+        self.learning_rate = learning_rate
+        self.weights = None     # Хранение весов модели
+    
+    def __str__(self):
+        return f"MyLogReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"

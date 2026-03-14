93604dc86c6edb58b0f7a6b95969e283b2ea2504
Karabaraboo <toto-93@yandex.ru>
2026-03-15 00:16:29 +0300

    Добавлены методы fit и build_tree

--- Tree_regression.py
+++ Tree_regression.py
@@ -12,12 +12,15 @@ class Node():
 
 class MyTreeReg():
     def __init__(self, 
-                 max_depth=5,                   # максимальная глубина
-                 min_samples_split=2,           # минимальное количество объектов в листе
-                 max_leafs=20):                 # максимальное количество листьев
+                 max_depth=5,                   # максимальная глубина (без учёта листьев)
+                 min_samples_split=2,           # минимальное количество объектов в узле, когда ещё можно разбить
+                 max_leafs=20,                  # максимальное количество листьев
+                 verbose=False):
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
         self.max_leafs = max_leafs
+        self.leafs_cnt = 0
+        self.verbose = verbose
 
     def __str__(self):
         description = []
@@ -29,7 +32,7 @@ class MyTreeReg():
     
     def get_best_split(self, X: pd.DataFrame, y: pd.Series, features: np.ndarray=None) -> tuple:
         if isinstance(X, pd.DataFrame):
-            features = X.columns.values
+            features = X.columns.tolist()
             X = X.to_numpy()
         if isinstance(y, pd.Series):
             y = y.to_numpy()
@@ -39,12 +42,12 @@ class MyTreeReg():
         best_gain = -1
         best_split = None
 
-        for column_idx in range(features.size):
+        for column_idx in range(len(features)):
             # MSE в текущем узле
             MSE_node = self.mse(y)
 
             # Составление списка разделителей
-            sorted_values = np.sort(X[:, column_idx])
+            sorted_values = np.unique(X[:, column_idx])         # Возвращает уникальные значения и уже отсортированные
             separators = (sorted_values[1:] + sorted_values[:-1]) / 2
 
             for sep in separators:
@@ -64,6 +67,49 @@ class MyTreeReg():
                     best_split = sep
         
         return (best_feature, best_split, best_gain)
+    
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+        features = X.columns.tolist()
+        X = X.to_numpy()
+        y = y.to_numpy()
+
+        self.leafs_cnt = 0      # На случай повторного вызова fit
+
+        self.tree = self.build_tree(X, y, features, np.arange(X.shape[0]), 1)
+
+    def build_tree(self, X: pd.DataFrame, y: pd.Series, features: list, indices: np.ndarray, depth: int):
+        # Условие на лист
+        condition_1 = depth > self.max_depth                                # по глубине (без листьев)
+        condition_2 = y.size < max(2, self.min_samples_split)               # по числу элементов в узле, но не меньше 1
+        condition_3 = self.leafs_cnt + 1 >= self.max_leafs and depth > 1    # по числу листьев
+        
+        
+        if condition_1 or condition_2 or condition_3:
+            if self.verbose:  
+                print("*********\nУсловия на лист")  
+                print(f"Условие по глубине: {condition_1}, Условие по числу элементов: {condition_2}, Условие по числу листов: {condition_3}")
+            
+            # Если условие выполняется, то это лист
+            self.leafs_cnt += 1
+
+            return Node(value=y[indices].mean())
+        
+        best_split = self.get_best_split(X, y, features)
+
+        left_msk = X[indices, features.index(best_split[0])] <= best_split[1]
+        if self.verbose:
+            print('**************\nРазбиение узла')
+            print(f"фича: {best_split[0]}, разделитель {best_split[1]}")
+            print(f"left_msk = {left_msk}")
+
+        tree_left = self.build_tree(X, y, features, indices[left_msk], depth + 1)
+        tree_right = self.build_tree(X, y, features, indices[~left_msk], depth + 1)
+        
+        return Node(feature=best_split[0],
+                    threshold=best_split[1],
+                    tree_left=tree_left,
+                    tree_right=tree_right)
+
 
     @staticmethod
     def mse(y: np.ndarray) -> int:

70253fef746f6ac901622381be83c01a1e66572f
Karabaraboo <toto-93@yandex.ru>
2026-03-25 08:44:33 +0300

    Добавлен расчёт feature importance

--- Tree_regression.py
+++ Tree_regression.py
@@ -15,12 +15,15 @@ class MyTreeReg():
                  max_depth=5,                   # максимальная глубина (без учёта листьев)
                  min_samples_split=2,           # минимальное количество объектов в узле, когда ещё можно разбить
                  max_leafs=20,                  # максимальное количество листьев
-                 verbose=False):
+                 verbose=False,
+                 bins=None):
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
         self.max_leafs = max_leafs
         self.leafs_cnt = 0
         self.verbose = verbose
+        self.bins = bins
+        self.fi = {}
 
     def __str__(self):
         description = []
@@ -47,8 +50,11 @@ class MyTreeReg():
             MSE_node = self.mse(y)
 
             # Составление списка разделителей
-            sorted_values = np.unique(X[:, column_idx])         # Возвращает уникальные значения и уже отсортированные
-            separators = (sorted_values[1:] + sorted_values[:-1]) / 2
+            if self.bins:
+                separators = self.thresholds[column_idx]
+            else:
+                sorted_values = np.unique(X[:, column_idx])         # Возвращает уникальные значения и уже отсортированные
+                separators = (sorted_values[1:] + sorted_values[:-1]) / 2
 
             for sep in separators:
                 left_msk = X[:, column_idx] <= sep
@@ -73,6 +79,14 @@ class MyTreeReg():
         X = X.to_numpy()
         y = y.to_numpy()
 
+        # Оказалось, что pd.nunique работает быстрее np.unique.size, т.к не требует сортировки.
+        # Но для поиска разделителей всё равно нужны уникальные значения и отсортированные
+        if self.bins:
+            self.get_separators(X)      # Возвращает self.thresholds - список разделителей для столбцов
+
+        # Создание словаря с важностью фичей. Все значения = 0
+        self.fi = dict.fromkeys(features, 0)
+
         self.leafs_cnt = 0      # На случай повторного вызова fit
 
         self.tree = self.build_tree(X, y, features, np.arange(X.shape[0]), 1)
@@ -82,7 +96,6 @@ class MyTreeReg():
         condition_1 = depth > self.max_depth                                # по глубине (без листьев)
         condition_2 = y[indices].size < max(2, self.min_samples_split)               # по числу элементов в узле, но не меньше 1
         condition_3 = self.leafs_cnt + potential > self.max_leafs and depth > 1    # по числу листьев
-        
         
         if condition_1 or condition_2 or condition_3:
             if self.verbose:  
@@ -99,21 +112,35 @@ class MyTreeReg():
 
         left_msk = X[indices, features.index(best_split[0])] <= best_split[1]
         condition_4 = len(indices[left_msk]) == 0 or len(indices[~left_msk]) == 0
-        if self.verbose:
-            print('**************\nРазбиение узла')
-            print(f"best_split: {best_split}")
-            
-            print("*********\nУсловия на лист")  
-            print(f"Условие на пустое разбиение: {condition_4}")
-            print(f"{self.leafs_cnt + 1}-ый лист, значение {y[indices].mean()}")
 
         if condition_4:
             '''Это лист.
             Если get_best_split возвращает такое разбиение, что вся выборка относится либо к левому, либо к правому дереву,
             то когда уйдём в построение этой части, мы получаем ту же самую ситуацию. Будет зацикленная рекурсия.
             Поэтому пришлось проверять это условие здесь'''
+            if self.verbose:
+                print('**************\nРазбиение узла')
+                print(f"best_split: {best_split}")
+                
+                print("*********\nУсловия на лист")  
+                print(f"Условие на пустое разбиение: {condition_4}")
+                print(f"{self.leafs_cnt + 1}-ый лист, значение {y[indices].mean()}")
+            
             self.leafs_cnt += 1
             return Node(value = y[indices].mean())
+        
+        if self.bins:
+            for column_idx in range(X.shape[1]):
+                if (np.any((X[indices, column_idx].min() < self.thresholds[column_idx]) & 
+                           (X[indices, column_idx].max() > self.thresholds[column_idx]))):
+                    # Значит, элементы X в разных бинах
+                    break
+                else:
+                    # Это лист
+                    return Node(value = y[indices].mean())
+
+        # Учёто важности фичи
+        self.fi[best_split[0]] += indices.size / X.shape[0] * best_split[2]
 
         tree_left = self.build_tree(X, y, features, indices[left_msk], depth + 1, potential + 1)
         tree_right = self.build_tree(X, y, features, indices[~left_msk], depth + 1, potential)
@@ -162,6 +189,24 @@ class MyTreeReg():
         
         return pd.Series(prediction)
 
+    def get_separators(self, X: np.ndarray):
+        # Пустой список для разделителей
+        self.thresholds = [None] * X.shape[1]   
+
+        # Поиск разделителей
+        for column_idx in range(X.shape[1]):
+            unique_sorted = np.unique(X[:, column_idx])
+
+            if unique_sorted.size <= self.bins:    # Ищем разделители, как среднее между уникальными значениями
+                self.thresholds[column_idx] = (unique_sorted[1:] + unique_sorted[:-1]) / 2
+            else:
+                hist, bin_edges = np.histogram(X[:, column_idx], bins=self.bins)
+                self.thresholds[column_idx] = bin_edges
+
     @staticmethod
     def mse(y: np.ndarray) -> int:
-        return ((y - y.mean())**2).mean()
+        if y.size:
+            return ((y - y.mean())**2).mean()
+        else:
+            return 0
+            
